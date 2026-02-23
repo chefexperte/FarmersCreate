fa9ef8bfeeccb0c43eadbbef2caef9099b2accb4
chefexperte <aaron+git@chefexperte.de>
2026-02-23 20:11:21 +0100

    Update compatibility to newer create fly versions. Change custom ingredient handling (they work but are displayed incorrectly by JEI). Update mod version to 2.0

--- src/main/java/de/chefexperte/farmersCreate/mixin/client/RecipeManagerMixin.java
+++ src/main/java/de/chefexperte/farmersCreate/mixin/client/RecipeManagerMixin.java
@@ -3,8 +3,8 @@ package de.chefexperte.farmersCreate.mixin.client;
 import com.zurrtum.create.content.kinetics.deployer.DeployerApplicationRecipe;
 import com.zurrtum.create.content.kinetics.mixer.MixingRecipe;
 import com.zurrtum.create.content.processing.recipe.HeatCondition;
+import com.zurrtum.create.content.processing.recipe.ProcessingOutput;
 import com.zurrtum.create.content.processing.recipe.SizedIngredient;
-import com.zurrtum.create.infrastructure.fluids.FluidStack;
 import de.chefexperte.farmersCreate.FarmersCreate;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
@@ -24,6 +24,7 @@ import vectorwing.farmersdelight.common.registry.ModRecipeTypes;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 @Mixin(RecipeManager.class)
 public class RecipeManagerMixin {
@@ -39,7 +40,9 @@ public class RecipeManagerMixin {
         System.out.println("Found " + cuttingBoardRecipes.size() + " Farmer's Delight Cutting Board Recipes");
         for (var cuttingBoardRecipeHolder : cuttingBoardRecipes) {
             var cuttingBoardRecipe = cuttingBoardRecipeHolder.value();
-            var cuttingRecipe = new DeployerApplicationRecipe(cuttingBoardRecipe.getResults().getFirst(), true, cuttingBoardRecipe.getInput(), cuttingBoardRecipe.getTool());
+            var processingOutputs = cuttingBoardRecipe.getRollableResults().stream().map(
+                    result -> new ProcessingOutput(result.stack().getItem(), result.stack().getCount(), result.chance())).toList();
+            var cuttingRecipe = new DeployerApplicationRecipe(processingOutputs, true, cuttingBoardRecipe.getInput(), cuttingBoardRecipe.getTool());
             var namespace = cuttingBoardRecipeHolder.id().identifier().getNamespace();
             var path = cuttingBoardRecipeHolder.id().identifier().getPath();
             var newId = Identifier.fromNamespaceAndPath(FarmersCreate.MOD_ID, "cutting/" + namespace + "/" + path);
@@ -55,41 +58,10 @@ public class RecipeManagerMixin {
         }
         for (var cookingRecipeHolder : cookingRecipes) {
             var cookingRecipe = cookingRecipeHolder.value();
-            var recipeId = cookingRecipeHolder.id().identifier().toString();
-            var skipRecipe = false;
-            var cleanInput = new ArrayList<Ingredient>(cookingRecipe.input().size());
-            // Check ingredients for CustomingredientImpl because ZurrTum Create fork does not support it for 1.21.11
-            for (var ingredient : cookingRecipe.input()) {
-                var customIngredient = ingredient.getCustomIngredient();
-                if (customIngredient == null) {
-                    cleanInput.add(ingredient);
-                    continue;
-                }
-                // Filter out known recipes, they have been manually added
-                switch (recipeId) {
-                    case "farmersdelight:cooking/bone_broth":
-                        // Does not work, idk why
-                        //cleanInput.add(Ingredient.of(BuiltInRegistries.ITEM.getOrThrow(FarmersCreate.BONE_BROTH_INGREDIENTS_TAG)));
-                        skipRecipe = true;
-                        break;
-                    case "farmersdelight:cooking/dumplings":
-                        // Does not work, idk why
-                        //cleanInput.add(Ingredient.of(BuiltInRegistries.ITEM.getOrThrow(FarmersCreate.DUMPLINGS_INGREDIENTS_TAG)));
-                        skipRecipe = true;
-                        break;
-                    default:
-                        System.out.println("The \"" + recipeId + "\" recipe uses custom ingredients, which is not compatible with ZurrTums Create fork for 1.21.11. Please report this to the FarmersCreate developer.");
-                        skipRecipe = true;
-                }
-                if (skipRecipe) {
-                    break;
-                }
-            }
-            if (skipRecipe) {
-                continue;
-            }
-            var ingredients = SizedIngredient.of(cleanInput);
-            var mixingRecipe = new MixingRecipe(cookingRecipe.result(), FluidStack.EMPTY, HeatCondition.HEATED, new ArrayList<>(), ingredients);
+            var ingredients = SizedIngredient.of(cookingRecipe.input());
+            var time = cookingRecipe.getCookTime();
+            var output = List.of(new ProcessingOutput(cookingRecipe.result()));
+            var mixingRecipe = new MixingRecipe(time, output, List.of(), HeatCondition.HEATED, new ArrayList<>(), ingredients);
             var namespace = cookingRecipeHolder.id().identifier().getNamespace();
             var path = cookingRecipeHolder.id().identifier().getPath();
             var newId = Identifier.fromNamespaceAndPath(FarmersCreate.MOD_ID, "cooking/" + namespace + "/" + path);
